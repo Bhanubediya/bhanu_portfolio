92273649250d07d257c82feadab2f111d297bbad
Bhanu Bediya <bhanu15599@mastek.com>
2024-03-16 03:43:12 +0530

    removed routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
   useEffect(() => {
     AOS.init();
-  }, [])
+  }, []);
   return (
     <>
-    <BrowserRouter basename="/bhanu_portfolio">
-    <Header/>
-    <Routes>
-      <Route exact path='/bhanu_portfolio' element={<Home/>}></Route>
-    </Routes>
-    <Footer/>
-    </BrowserRouter>
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 }
